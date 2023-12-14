8a065422a4d21fad815943be895c82b1b0e99f51
Richard Buckley <42151145+rhbuckley@users.noreply.github.com>
2023-12-14 08:39:57 -0500

    style: update functions

--- convert.py
+++ convert.py
@@ -43,7 +43,7 @@ class AsciiImageConverter:
         self.normalization_method = kwargs.get("normalization", "luminance")
 
     def normalize_image(self, input_image: np.ndarray) -> np.ndarray:
-        """ normalize an image - can change mode here, but once we find the best mode... """
+        """ normalize an image, using the norm method passed """
         conv = ImageNormalization(self.normalization_method, self.image_size)
         return conv.normalize(input_image)
 
@@ -54,19 +54,21 @@ class AsciiImageConverter:
 
     def convert_image(self, image: np.ndarray) -> np.ndarray:
         """
-        This is the main function that converts an image to ascii.
-
-        if AsciiImageConverter was passed color, it will return (char, r, g, b).
-        otherwise, it will return (char,)
+        This is the main function that converts an image to ascii. Returns an image
+        in a 3D array... if AsciiImageConverter was passed color, each pixel will have
+        the shape (char, r, g, b). If there was no color passed, each pixel will have
+        the shape (char, ).
 
         :param image: the image to convert to ascii
         :return: the converted image (height, width, 1), or (height, width, 4)
         :return:
         """
         clean_image = self.normalize_image(image)
-        resized_image = image_resize(image, *self.image_size)
         return_shape = (*clean_image.shape, 4 if self.color else 1)
         new_image = np.empty(return_shape, dtype=object)
+
+        # we will need to resize our original image to sample colors from
+        resized_image = image_resize(image, *self.image_size)
 
         for idx in np.ndindex(clean_image.shape[:2]):
             pixel = clean_image[idx]
@@ -79,7 +81,7 @@ class AsciiImageConverter:
                 *(resized_image[idx] if self.color else []),
             ]
 
-        # flip around axis=0 to fix mirroring effect
+        # flip around axis=0 to fix mirroring effect by camera
         new_image = np.flip(new_image, axis=1)
         return new_image
 
@@ -88,22 +90,20 @@ class AsciiImageConverter:
         converted_image = self.convert_image(image)
 
         if self.color:
-            return "\n".join([
-                "".join([
-                    Fore.RGB(*pixel) + char + Style.RESET
-                    for char, *pixel in row
-                ])
-                for row in converted_image
-            ])
+            wrap_pixel = lambda c, pix: Fore.RGB(*pix) + c + Style.RESET
+        else:
+            wrap_pixel = lambda c, pix: c
 
         return "\n".join([
-                "".join([char for (char, *_) in row])
-                for row in converted_image
-            ])
+            "".join(wrap_pixel(char, pixel) for char, *pixel in row)
+            for row in converted_image
+        ])
 
-    def convert_image_from_path(self, path: str):
+    def convert_image_from_path(self, path: str, to_terminal: bool = False):
         """ convert an image from a path to ascii """
         image = cv2.imread(path, cv2.IMREAD_COLOR)
         if image is None:
             raise IOError(f"Could not read image from path: {path}")
+        if to_terminal:
+            return self.convert_image_to_terminal(image)
         return self.convert_image(image)
